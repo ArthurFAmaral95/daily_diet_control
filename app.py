40353f109b3039fdf0c7eeb47450a691cd5902e5
Arthur Amaral <arthuramaral6@gmail.com>
2025-06-22 11:08:19 -0300

    Login route

--- app.py
+++ app.py
@@ -34,6 +34,19 @@ def create_user():
 
   return jsonify({'message': 'Dados inválidos.'}), 400  
 
+@app.route('/login', methods=['POST'])
+def login():
+  data = request.json
+  username = data.get('username')
+  password = data.get('password')
+
+  if username and password:
+    user = User.query.filter_by(username=username).first()
+    if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+      login_user(user)
+      return jsonify({'message': f'Autenticação realizada com sucesso. Usuário {user.username} logado.'})
+
+  return jsonify({'message': 'Credenciais inválidas'}), 400
 
 if __name__ == '__main__':
   app.run(debug=True)
