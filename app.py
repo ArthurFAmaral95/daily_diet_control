f55e3a82f70857e76fcddb8eb3753fd3ed2b25fb
Arthur Amaral <arthuramaral6@gmail.com>
2025-06-22 10:56:51 -0300

    Create_user route

--- app.py
+++ app.py
@@ -19,6 +19,21 @@ login_manager.login_view = 'login'
 def load_user(user_id):
   return User.query.get(user_id)
 
+@app.route('/user', methods=['POST'])
+def create_user():
+  data = request.json
+  username = data.get('username')
+  password = data.get('password')
+
+  if username and password:
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    user = User(username=username, password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message':'Usuário criado com sucesso.', 'user_id': user.id})
+
+  return jsonify({'message': 'Dados inválidos.'}), 400  
+
 
 if __name__ == '__main__':
   app.run(debug=True)
