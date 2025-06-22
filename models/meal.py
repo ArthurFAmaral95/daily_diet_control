4a2fabbe73818e5a1caa4aca5d54cb8af3c877a7
Arthur Amaral <arthuramaral6@gmail.com>
2025-06-21 21:38:07 -0300

    Set up models for tables in db

---
+++ models/meal.py
@@ -0,0 +1,9 @@
+from database import db
+import datetime
+
+class Meal(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  meal_name = db.Column(db.String(80), nullable=False)
+  meal_description = db.Column(db.String(255))
+  date_time = db.Column(db.DateTime(timezone=True), default=lambda: datetime.datetime.now(datetime.timezone.utc))
+  diet_meal = db.Column(db.Boolean, default=True, nullable=False)
