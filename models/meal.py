a595f492423f9cc506cdf97f69a689cc5d30fb5e
Arthur Amaral <arthuramaral6@gmail.com>
2025-06-22 10:39:53 -0300

    àdding user_id column to meal table

--- models/meal.py
+++ models/meal.py
@@ -2,8 +2,11 @@ from database import db
 import datetime
 
 class Meal(db.Model):
+  __tablename__ = 'meal'
+
   id = db.Column(db.Integer, primary_key=True)
   meal_name = db.Column(db.String(80), nullable=False)
   meal_description = db.Column(db.String(255))
   date_time = db.Column(db.DateTime(timezone=True), default=lambda: datetime.datetime.now(datetime.timezone.utc))
   diet_meal = db.Column(db.Boolean, default=True, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
--- models/user.py
+++ models/user.py
@@ -2,6 +2,10 @@ from database import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+  __tablename__ = 'user'
+
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), nullable=False, unique=True)
   password = db.Column(db.String(80), nullable=False)
+
+  meals = db.relationship('Meal', backref='user', lazy=True)
